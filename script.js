4f1968f026fc1a01c5855131bb8c9efc9a488a3a
Chenai881 <43444283+Chenai881@users.noreply.github.com>
2019-03-26 09:39:06 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,10 @@
+$("button").click(function() {
+            var color = $("#color").val();
+            var legs = $("#legs").val();
+            if (#color === pink && legs > 3) {
+                // apple jack//
+            } else if (#color === purple && legs > 3) {
+                //pinkie pie//
+            }
+            });
+            
